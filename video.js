743f0e1bb89235ec1a07cf2dc8f9ab024decdd6d
Ben Thomas <b.thomas@sheffield.ac.uk>
2018-02-11 22:56:39 +0000

    something

--- video.js
+++ video.js
@@ -1,11 +1,17 @@
 
 var div = document.getElementById('data')
 div.innerHTML = JSON.stringify(navigator.mediaDevices)
+var selects = document.querySelector('select')
 
 navigator.mediaDevices.enumerateDevices()
 .then(function(devices){
 
   devices.forEach(function(device){
+    if(device.kind == 'videoInput'){
+        selects.forEach(function(elem){
+          elem.append(device.label)
+        })     
+    }
     console.log(device)
   })
 
@@ -22,6 +28,13 @@ navigator.mediaDevices.getUserMedia({
         video:true,
         audio:true
 }).then(function(stream){
+  console.log(stream)
+  var video1 = document.getElementById('video1')
+   var video2 = document.getElementById('video2')
+  video1.srcObject = stream
+  video1.play()
+   video2.srcObject = stream
+  video2.play()
 
 
 })
